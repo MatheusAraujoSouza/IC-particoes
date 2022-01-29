a32f7d70823521f52d2c158d3bad81e1ccbc5655
Matheus Araujo Souza <63359384+MatheusAraujoSouza@users.noreply.github.com>
2022-01-29 17:12:10 -0300

    Pequenas dicas de nomenclatura
    Algumas dicas que podem ajudar outros desenvolvedores na leitura do seu código.

--- particao.m
+++ particao.m
@@ -1,11 +1,11 @@
-function p = particao(n)
+function p = particao(n)%Use verbos no infinitivo para nomer nomes de funções, isso ajuda no entendimento.
     %Valores iniciais
-    lzero = floor(n/2);
-    r = zeros(lzero,1);
+    lzero = floor(n/2); %evite chamar de lzero isso não informa muita coisa sobre a variavel e também não chame de r ou p. Seu codigo precisa ser entendivel para outros programadores
+    r = zeros(lzero,1); %para fazer isso use nomes de váriaveis que informem e tragam significado.
     r(1) = n-1;
     p = r(1)+1;
 
-    %Laco para somar D2, D3, ..., Dlzero
+    %Laco para somar D2, D3, ..., Dlzero 
     for l=2:lzero
         r(l) = n-2*l;
         d=D(l,r(l),n);
