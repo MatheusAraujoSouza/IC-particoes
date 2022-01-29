1560589121f458b60b7872a8e3bb0d71d60a8adb
Matheus Araujo Souza <63359384+MatheusAraujoSouza@users.noreply.github.com>
2022-01-29 17:54:57 -0300

    Nomes de variáveis e de funções

--- D.m
+++ D.m
@@ -1,6 +1,7 @@
-function somatorio = D(l,rl,n)
-    j = 1;
+function somatorio = D(l,rl,n) %Outra coisa d.m ou tj.m isso é duro sabe, porque preciso entrar no metodo/função e ver o que faz e o nome dela nem me explica nada 
+                                %somatorio não é um verbo no infinitivo está mais para uma saída de função tipo var somatorio = SomarElmentosDoArray(.....)
+    j = 1; %usa-se j apenas para loops for assim não é correto.
     somatorio = 0;
-    t = zeros(l-1,1);
+    t = zeros(l-1,1); % t não me traz informação nenhuma do que essa variavel faz, eu teria que buscar a resposta da saída de zeros e deduzir? 
     somatorio = soma(t,l,rl,somatorio,j,n);
 end
